5c7a20b1cb798d57f505f745669089aa47cc948c
minjeongHEO <hmjoung9770@gmail.com>
2024-04-16 16:26:24 +0900

    feat: 초기 화면 post css 설정

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
-import '../css/components/App.css';
+import styles from '../css/components/App.module.css';
 import Header from './Header';
 import Section from './Section';
 
 function App() {
   return (
-    <>
+    <div id={styles.wrap}>
       <Header />
       <Section />
-    </>
+    </div>
   );
 }
 
