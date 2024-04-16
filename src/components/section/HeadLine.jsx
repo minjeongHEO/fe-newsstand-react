4dabd74d7f643fe10b352a4cb7c77afc522cdfb1
minjeongHEO <hmjoung9770@gmail.com>
2024-04-16 23:56:34 +0900

    WIP: 헤드라인 컴포넌트 작업 중 - json파일 fetch 작업 중....

--- src/components/section/HeadLine.jsx
+++ src/components/section/HeadLine.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import styles from '../../css/components/section/HeadLine.module.scss';
 
-export default function HeadLine() {
-    return <div>헤드라인입니다</div>;
+export default function HeadLine({ children }) {
+    return (
+        <section className={styles.main__section__headline}>
+            <div className={styles.headline__container}></div>
+        </section>
+    );
 }
--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-import HeadLine from "./HeadLine";
-import News from "./News";
+import { useEffect, useState } from 'react';
+import HeadLine from './HeadLine';
+import News from './News';
+// import headlineData from '../../json/headlinesData.json';
+// import pressData from '../../json/pressData.json';
 
 export default function Section() {
+    const [newsdata, setNewsData] = useState({ headline: {}, press: {} });
+
+    const fetchData = async () => {
+        const headlineFilePath = '../../json/headlinesData.json';
+        const pressFilePath = '../../json/pressData.json';
+        const headlineResponse = await fetch(headlineFilePath);
+        const pressResponse = await fetch(pressFilePath);
+
+        if (headlineResponse.ok !== true || pressResponse.ok !== true) return;
+
+        const headlineData = await headlineResponse.json();
+        const pressData = await pressResponse.json();
+
+        setNewsData({ headline: headlineData, press: pressData });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
-        <>
-            <HeadLine></HeadLine>
-            <News></News>
-        </>
+        <main>
+            <HeadLine data={newsdata.headline} />
+            <News data={newsdata.press} />
+        </main>
     );
 }
