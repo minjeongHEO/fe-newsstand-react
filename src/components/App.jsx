32370715b6cc6c5126141240ac2914177afbf3a7
minjeongHEO <hmjoung9770@gmail.com>
2024-04-16 16:17:54 +0900

    feat: 초기 컴포넌트 셋팅

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import '../css/components/App.css';
+import Header from './Header';
+import Section from './Section';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Section />
+    </>
+  );
+}
+
+export default App;
