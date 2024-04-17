c7b4b63721c1cafff5d616fc0242a04ddf160ca3
minjeongHEO <hmjoung9770@gmail.com>
2024-04-17 10:43:03 +0900

    fix: 파일 경로 수정

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from 'react';
 import HeadLine from './HeadLine';
 import News from './News';
-// import headlineData from '../../json/headlinesData.json';
-// import pressData from '../../json/pressData.json';
 
 export default function Section() {
     const [newsdata, setNewsData] = useState({ headline: {}, press: {} });
 
     const fetchData = async () => {
-        const headlineFilePath = '../../json/headlinesData.json';
-        const pressFilePath = '../../json/pressData.json';
+        const headlineFilePath = 'data/headlineData.json';
+        const pressFilePath = 'data/pressData.json';
         const headlineResponse = await fetch(headlineFilePath);
         const pressResponse = await fetch(pressFilePath);
 
