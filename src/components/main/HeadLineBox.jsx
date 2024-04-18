1f07d2b58f28cd2755e352594d7fccd4773b2f70
minjeongHEO <hmjoung9770@gmail.com>
2024-04-19 01:34:38 +0900

    feat: 헤드라인 롤링 애니메이션

--- src/components/main/HeadLineBox.jsx
+++ src/components/main/HeadLineBox.jsx
@@ -5,27 +5,26 @@ import RollingBox from './RollingBox';
 export default function HeadLineBox({ section, divideData }) {
     if (divideData.length === 0) return;
 
-    const dataLength = divideData[0].length;
+    const dataLength = divideData[section].length;
     const animationDuration = 3000; // 애니메이션 지속 시간 (3초)
     const [order, setOrder] = useState([0, 1, 2]);
-    const [pauseAni, setPauseAni] = useState(false);
+    const [toggleAni, setToggleAni] = useState(false);
 
     useEffect(() => {
         const interval = setInterval(() => {
             setOrder((currentOrder) => currentOrder.map((index) => (index + 1) % dataLength));
+            setToggleAni((curAni) => !curAni);
         }, animationDuration);
 
-        setPauseAni((curAni) => !curAni);
         return () => clearInterval(interval);
-    }, [dataLength]);
+    }, [dataLength, divideData.length]);
 
     return (
         <div className={`${styles.contents} ${styles.section}${section}`}>
-            {/* {Array.from({ length: order.length }).map((_, idx) => { */}
             {order.map((contentIndex, idx) => {
                 const content = divideData[section][contentIndex];
                 return (
-                    <div key={idx} className={`${styles.rolling_box} ${styles.text_move_up} ${pauseAni ? styles.text_move_up_paused : ''}`}>
+                    <div key={idx} className={`${styles.rolling_box} ${toggleAni ? styles.text_move_up_paused : styles.text_move_up}`}>
                         <div className={styles.press_name}>
                             <a href={content.newsLink} target="_blank">
                                 {content.newsName}
