717a54ab5ba812af627c3c6552c79027a7d50cf0
minjeongHEO <hmjoung9770@gmail.com>
2024-04-22 00:15:59 +0900

    refactor: 동적으로 grid CSS 변경

--- src/components/main/GridNews.jsx
+++ src/components/main/GridNews.jsx
@@ -3,15 +3,20 @@ import styles from './GridNews.module.scss';
 import GridLine from './GridLine';
 
 export default function GridNews({ row, col, newsData, page }) {
-    console.log('newsData[0]: ', newsData[0]);
+    const gridStyle = {
+        display: 'grid',
+        gridTemplateColumns: `repeat(${col}, 1fr)`,
+        height: '100%',
+    };
+
     return (
         <div className={styles.gridContainer}>
             <GridLine row={row} col={col} />
-            <div className={styles.media__grid_type__container}>
+            <div className={styles.media__grid_type__container} style={gridStyle}>
                 {newsData[page].map((press) => (
                     <div key={press.id}>
                         <a href="#" className={styles['media__subscription-news-view']}>
-                            <img src={press.logoImageSrc} alt={press.pressName} className={styles['media__grid_type__news_logo']}></img>
+                            <img src={press.logoImageSrc} alt={press.pressName} className={styles['media__grid_type__news_logo']} height="20"></img>
                         </a>
                     </div>
                 ))}
