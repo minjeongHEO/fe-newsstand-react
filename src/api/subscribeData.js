93cce0bc6c500f32de7a3caeca5a4ba044c81f7f
minjeongHEO <hmjoung9770@gmail.com>
2024-04-23 18:03:45 +0900

    feat: 구독하기 클릭 시 insert

---
+++ src/api/subscribeData.js
@@ -0,0 +1,24 @@
+/**
+ * 구독할 데이터 insert
+ * @param {Object} - {pressName, logoImageSrc}
+ * @returns {Object} - { result: bool, msg: 'error message' };
+ */
+export const insertSubscribeData = async (subscriptionData) => {
+    let insertResult = { result: false, msg: '' };
+
+    const response = await fetch(`${import.meta.env.VITE_JSON_SERVER}/subscribe`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(subscriptionData),
+    });
+
+    await response.json();
+
+    // 저장 성공 여부 확인
+    if (response.ok) insertResult = { result: true, msg: 'Data saved successfully.' };
+    else insertResult = { result: false, msg: 'Data save failure.' };
+
+    return insertResult;
+};
