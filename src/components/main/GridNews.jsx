4b603bd2075da4acb287d48d0393b40ec682d0c5
minjeongHEO <hmjoung9770@gmail.com>
2024-04-23 20:42:23 +0900

    fix: 구독하기 시 id값 지정

--- src/components/main/GridNews.jsx
+++ src/components/main/GridNews.jsx
@@ -18,14 +18,9 @@ export default function GridNews({ newsData, page, setPage }) {
     const prevArrowClick = () => setPage((prev) => ({ ...prev, grid: prev.grid - 1 }));
     const nextArrowClick = () => setPage((prev) => ({ ...prev, grid: prev.grid + 1 }));
 
-    const setSubscribe = async (e) => {
-        const { target } = e;
-        const imgTarget = target.parentNode.children[0].children[0];
-        const pressName = imgTarget.alt;
-        const logoImageSrc = imgTarget.src;
+    const unSubscribe = async (subscribeObj) => {};
 
-        const subscribeObj = { pressName, logoImageSrc };
-
+    const subscribe = async (subscribeObj) => {
         const insertResult = await insertSubscribeData(subscribeObj);
         if (insertResult.result) {
             const selectAllResult = await selectAllSubscribeData();
@@ -33,16 +28,34 @@ export default function GridNews({ newsData, page, setPage }) {
         }
     };
 
+    const setSubscribe = async ({ target }) => {
+        const id = target.parentNode.id;
+        const imgTarget = target.parentNode.children[0].children[0];
+        const pressName = imgTarget.alt;
+        const logoImageSrc = imgTarget.src;
+
+        const subscribeObj = { id, pressName, logoImageSrc };
+
+        //구독하기
+        if (target.getAttribute('subscribe') === 'true') subscribe(subscribeObj);
+        //해지하기
+        if (target.getAttribute('subscribe') === 'false') unSubscribe(subscribeObj);
+    };
+
     return (
         <div className={styles.gridContainer}>
             <GridLine />
             <div className={styles.media__grid_type__container} style={gridStyle}>
                 {newsData[page].map((press) => (
-                    <div key={press.id}>
+                    <div key={press.id} id={press.id}>
                         <a href="#" className={styles['media__subscription-news-view']}>
                             <img src={press.logoImageSrc} alt={press.pressName} className={styles['media__grid_type__news_logo']}></img>
                         </a>
-                        <button className={styles['media__grid_type__subscribe_btn']} onClick={setSubscribe}>
+                        <button
+                            className={styles['media__grid_type__subscribe_btn']}
+                            onClick={setSubscribe}
+                            subscribe={subscribes.find(({ pressName }) => pressName === press.pressName) ? 'true' : 'false'}
+                        >
                             {subscribes.find(({ pressName }) => pressName === press.pressName) ? '- 해지하기' : '+ 구독하기'}
                         </button>
                     </div>
