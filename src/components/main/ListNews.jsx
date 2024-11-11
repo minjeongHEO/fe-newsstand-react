2ff8563e50eca428e90f4381d53c096614abbcc8
minjeongHEO <hmjoung9770@gmail.com>
2024-11-12 01:28:06 +0900

    feat: 리스트형 페이지 넘기기

--- src/components/main/ListNews.jsx
+++ src/components/main/ListNews.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styles from './ListNews.module.scss';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 
 export default function ListNews({ newsData, tabType }) {
   const [categories, setCategories] = useState([]);
@@ -7,7 +8,7 @@ export default function ListNews({ newsData, tabType }) {
   const [currentPageNewsData, setCurrentPageNewsData] = useState(null);
 
   const [page, setPage] = useState(0);
-  const [clickedCategory, setClickedCategory] = useState(0);
+  const [clickedCategoryIndex, setClickedCategoryIndex] = useState(0);
 
   const NEWS_DATA_MAP = {
     ALL_PRESS: newsData.news,
@@ -38,59 +39,88 @@ export default function ListNews({ newsData, tabType }) {
   }, [tabType.subscribe, categories, newsData]);
 
   useEffect(() => {
-    if (!listNewsData || !listNewsData.length) return;
-    setCurrentPageNewsData(listNewsData[clickedCategory][page]);
-  }, [tabType.subscribe, listNewsData, clickedCategory, page]);
+    if (!listNewsData || !listNewsData[clickedCategoryIndex]) return;
+    setCurrentPageNewsData(listNewsData[clickedCategoryIndex][page]);
+  }, [tabType.subscribe, listNewsData, clickedCategoryIndex, page]);
+
+  const handleCategoryClick = (idx) => {
+    setClickedCategoryIndex(idx);
+    setPage(0);
+  };
+
+  const prevArrowClick = () => {
+    if (!page) {
+      setClickedCategoryIndex((prevIndex) => (!prevIndex ? categories.length - 1 : prevIndex - 1));
+      setPage(0);
+      return;
+    }
+    setPage((prevPage) => prevPage - 1);
+  };
+
+  const nextArrowClick = () => {
+    const maxPage = listNewsData[clickedCategoryIndex].length;
+    if (page + 1 === maxPage) {
+      setClickedCategoryIndex((prevIndex) => (prevIndex === categories.length - 1 ? 0 : prevIndex + 1));
+      setPage(0);
+      return;
+    }
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
-    <div>
-      <div className={styles.media__category_bar}>
-        {categories.map((category, idx) => {
-          return (
-            <div key={`category${idx}`}>
-              <span>{category}</span>
-            </div>
-          );
-        })}
-      </div>
+    <>
+      <LeftOutlined className={styles.angle_left} onClick={prevArrowClick} />
+      <div>
+        <div className={styles.media__category_bar}>
+          {categories.map((category, idx) => {
+            return (
+              <div key={`category${idx}`} onClick={() => handleCategoryClick(idx)}>
+                <span>{category}</span>
+                <span style={{ marginLeft: '0.5rem' }}>{clickedCategoryIndex === idx && `${page + 1} / ${listNewsData[idx]?.length || 0}`}</span>
+              </div>
+            );
+          })}
+        </div>
 
-      {currentPageNewsData && (
-        <>
-          <div className={styles.media__news_container}>
-            <div className={styles.media__news_logo}>
-              <a href="#">
-                <img src={currentPageNewsData.logoImageSrc} height="20" width="auto" alt={currentPageNewsData.pressName} />
-              </a>
-              <span class={styles.media__news_time}>{currentPageNewsData.editedTime}</span>
-              <button type="button" class={styles.media__news_subscribe_btn} aria-pressed="${pressedStatus}">
-                구독 버튼
-              </button>
-            </div>
-
-            <div class={styles.media__news_datas}>
-              <div class={styles.media__news__main}>
-                <a target="_blank" class={styles.media__news__main__link} href={currentPageNewsData.headline.href}>
-                  <img src={currentPageNewsData.headline.thumbnailSrc} alt={currentPageNewsData.headline.title} />
+        {currentPageNewsData && (
+          <>
+            <div className={styles.media__news_container}>
+              <div className={styles.media__news_logo}>
+                <a href="#">
+                  <img src={currentPageNewsData.logoImageSrc} height="20" width="auto" alt={currentPageNewsData.pressName} />
                 </a>
-                <a target="_blank" class={styles.media__news__main__head_line} href={currentPageNewsData.headline.href}>
-                  {currentPageNewsData.headline.title}
-                </a>
+                <span className={styles.media__news_time}>{currentPageNewsData.editedTime}</span>
+                <button type="button" className={styles.media__news_subscribe_btn} aria-pressed="${pressedStatus}">
+                  구독 버튼
+                </button>
               </div>
 
-              <ul class={styles.media__news__sub}>
-                {currentPageNewsData.sideNews.map(({ href, title }) => (
-                  <li>
-                    <a target="_blank" href={href}>
-                      {title}
-                    </a>
-                  </li>
-                ))}
-                <li> {currentPageNewsData.pressName} 언론사에서 직접 편집한 뉴스입니다.</li>
-              </ul>
+              <div className={styles.media__news_datas}>
+                <div className={styles.media__news__main}>
+                  <a target="_blank" className={styles.media__news__main__link} href={currentPageNewsData.headline.href}>
+                    <img src={currentPageNewsData.headline.thumbnailSrc} alt={currentPageNewsData.headline.title} />
+                  </a>
+                  <a target="_blank" className={styles.media__news__main__head_line} href={currentPageNewsData.headline.href}>
+                    {currentPageNewsData.headline.title}
+                  </a>
+                </div>
+
+                <ul className={styles.media__news__sub}>
+                  {currentPageNewsData.sideNews.map(({ href, title }, idx) => (
+                    <li key={idx}>
+                      <a target="_blank" href={href}>
+                        {title}
+                      </a>
+                    </li>
+                  ))}
+                  <li> {currentPageNewsData.pressName} 언론사에서 직접 편집한 뉴스입니다.</li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </div>
+      <RightOutlined className={styles.angle_right} onClick={nextArrowClick} />
+    </>
   );
 }
